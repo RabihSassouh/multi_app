b4c6a1a11b25c3adb7b8bc4a371bba57182f98ae
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-03-28 00:49:15 +0200

    added stickycontainer component where each not have its container and the user can edit or delete it

--- src/components/stickycontainer.jsx
+++ src/components/stickycontainer.jsx
@@ -1,21 +1,33 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// const StickyNote=({id,text,deleteNote, editNote})=>{
-//     const [text,setText]=useState(text);
-    
-//     const handleDelete =()=>{
-//         deleteNote(id);
-//     };
-//     const handleEdit=()=>{
-//         editNote(id,text);
-//     };
-//     return (
-//         <div >
-//             <input type="text" value={text}/>
-//             <button onClick={handleEdit}>Save</button>
-//             <button onClick={handleDelete}>Delete</button>
-//         </div>
-//     );
-// };
+const StickyContainer = ({ id, text, deleteNote, editNote }) => {
+  const [noteText, setNoteText] = useState(text);
 
-// export default StickyNote;
+  const handleDelete = () => {
+    deleteNote(id);
+  };
+  const handleEdit = () => {
+    editNote(id, noteText);
+  };
+  return (
+    <div>
+      {/* <div>
+        defaultSize={{ width: 200, height: 200 }}
+        minWidth={100}
+        minHeight={100}
+        maxHeight={500}
+        maxWidth={500}
+      </div> */}
+      <div>
+        <textarea
+          value={noteText}
+          onChange={(e) => setNoteText(e.target.value)}
+        />
+        <button onClick={handleEdit}>Edit</button>
+        <button onClick={handleDelete}>Delete</button>
+      </div>
+    </div>
+  );
+};
+
+export default StickyContainer;
--- src/pages/sticky-notes.jsx
+++ src/pages/sticky-notes.jsx
@@ -1,30 +1,42 @@
 import React, { useState } from "react";
+import StickyContainer from "../components/stickycontainer";
 
-const StickyNotes=()=>{
-    const [notes,setNotes]=useState([]);
-    const [id,setId]=useState(0);
+const StickyNotes = () => {
+  const [notes, setNotes] = useState([]);
+  const [id, setId] = useState(0);
 
-    const addNote=()=>{
-        setNotes([...notes,{id:id,text:''}]);
-        setId(id ++);
-    };
+  const addNote = () => {
+    setNotes([...notes, { id: id, text: "" }]);
+    setId(id + 1);
+  };
 
-    return(
-        <div>
-            <input type="text" placeholder="Add new note" />
-            <button onClick={addNote}>Add Note</button>
-            <div>
-                {notes.map((note)=>(
-                    <div>
-                        id={note.id}
-                        text={note.text}
+  const deleteNote = (id) => {
+    setNotes(notes.filter((note) => note.id !== id));
+  };
 
-                    </div>
-                )
-                )}
-            </div>
-        </div>
-    )
-}
+  const editNote = (id, newText) => {
+    setNotes(
+      notes.map((note) => (note.id === id ? { ...note, text: newText } : note))
+    );
+  };
+
+  return (
+    <div>
+      <input type="text" placeholder="Add new note" />
+      <button onClick={addNote}>Add Note</button>
+      <div>
+        {notes.map((note) => (
+          <StickyContainer
+            key={note.id}
+            id={note.id}
+            text={note.text}
+            deleteNote={deleteNote}
+            editNote={editNote}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default StickyNotes;
